804b324da2df282060b1999b2b74d0d188901ecd
seunghunbang <upi21x@gmail.com>
2023-06-07 21:28:30 +0900

    7/6/2023

--- practice14.py
+++ practice14.py
@@ -1,14 +1,18 @@
 import numpy as np
 
-a = np.array([.3, 2.9, 4.0])
 
-exp_a = np.exp(a)
-print(exp_a)
+# a = np.array([.3, 2.9, 4.0])
 
-sum_exp_a = np.sum(exp_a)
-print(sum_exp_a)
+def softmax(a):
+    exp_a = np.exp(a)
+    print(exp_a)
 
-y = exp_a / sum_exp_a
-print(y)
+    sum_exp_a = np.sum(exp_a)
+    print(sum_exp_a)
+
+    y = exp_a / sum_exp_a
+    print(y)
+
+    return y
 
 # 위의 코드는 softmax 함수를 구현한 것 입니다.
